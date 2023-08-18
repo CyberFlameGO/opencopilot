8fa1b4fc1caae4918a2292ccb7c2a1b2b2b19b46
Kaspar Peterson <kasparpeterson@hotmail.com>
2023-08-18 13:00:25 +0300

    Fixes e2e tests CI job

--- example.py
+++ example.py
@@ -1,20 +1,4 @@
-from typing import List
-
-from langchain.schema import Document
-
 from opencopilot import OpenCopilot
 
 copilot = OpenCopilot()
-# copilot.add_prompt("prompt_template.txt")
-copilot.add_local_files_dir("data")
-
-
-@copilot.data_loader
-def my_data() -> List[Document]:
-    return [Document(
-        page_content="Kristjan is from Latvia and is a frontend developer",
-        metadata={"source": "internet"}
-    )]
-
-
 copilot()
